6232845e70d6d7a6c9c0829134240a0485d30025
paul-f-maxson <paul.f.maxson@gmail.com>
2025-12-13 19:14:07 -0500

    Dyamic attributes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    // a "derived signal" is a function that accesses other signals
+    // we can use this to create reactive values that depend on the
+    // values of one or more other signals
+    let double_count = move || count.get() * 2;
+
+    view! {
+        <button
+            on:click=move |_| {
+                *set_count.write() += 1;
+            }
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count.get() % 2 == 1
+            class=("button-20", move || count.get() % 2 == 1)
+        >
+            "Click me"
+        </button>
+        // NOTE: self-closing tags like <br> need an explicit /
+        <br/>
+
+        // We'll update this progress bar every time `count` changes
+        <progress
+            // static attributes work as in HTML
+            max="50"
+
+            // passing a function to an attribute
+            // reactively sets that attribute
+            // signals are functions, so `value=count` and `value=move || count.get()`
+            // are interchangeable.
+            value=count
+        >
+        </progress>
+        <br/>
+
+        // This progress bar will use `double_count`
+        // so it should move twice as fast!
+        <progress
+            max="50"
+            // derived signals are functions, so they can also
+            // reactively update the DOM
+            value=double_count
+        >
+        </progress>
+        <p>"Count: " {count}</p>
+        <p>"Double Count: " {double_count}</p>
+    }
+}
+
 fn main() {
-    console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    leptos::mount::mount_to_body(App)
 }
